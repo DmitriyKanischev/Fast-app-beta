f18cea4a8773f83061ea65eeee2b3e8ce308da01
DmitriyKanischev <116942865+DmitriyKanischev@users.noreply.github.com>
2023-05-09 15:18:56 +0400

    sortArrow added, user.jsx deleted

--- src/components/TableHeader.jsx
+++ src/components/TableHeader.jsx
@@ -9,6 +9,20 @@ const TableHeader = ({onSort, selectedSort, columns}) => {
             onSort({path:item, order: 'asc'})
         }
     }
+    const renderSortArrow = (selectedSort, currentPath) => {
+        if(currentPath && selectedSort.path === currentPath) {
+            if(selectedSort.order === 'asc') {
+                return <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-caret-down-fill" viewBox="0 0 16 16">
+                            <path d="M7.247 11.14 2.451 5.658C1.885 5.013 2.345 4 3.204 4h9.592a1 1 0 0 1 .753 1.659l-4.796 5.48a1 1 0 0 1-1.506 0z"/>
+                        </svg>
+            } else {
+                return <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-caret-up-fill" viewBox="0 0 16 16">
+                            <path d="m7.247 4.86-4.796 5.481c-.566.647-.106 1.659.753 1.659h9.592a1 1 0 0 0 .753-1.659l-4.796-5.48a1 1 0 0 0-1.506 0z"/>
+                        </svg>
+            }
+        }
+        return null;
+    }
     return (
         <thead>
             <tr>
@@ -17,16 +31,9 @@ const TableHeader = ({onSort, selectedSort, columns}) => {
                         key={column} 
                         onClick={columns[column].path ? () =>handleSort(columns[column].path) : undefined} 
                         {...{role: columns[column].path && 'button'}}
-                        scope="col">{columns[column].name}
+                        scope="col">{columns[column].name} {renderSortArrow(selectedSort, columns[column].path)}
                     </th>
                 ))}
-                {/* <th onClick={() =>handleSort('name')} scope="col">Имя</th>
-                <th scope="col">Качества</th>
-                <th onClick={() =>handleSort('profession.name')} scope="col">Профессия</th>
-                <th onClick={() =>handleSort('completedMeetings')} scope="col">Встретился, раз</th>
-                <th onClick={() =>handleSort('rate')} scope="col">Оценка</th>
-                <th onClick={() =>handleSort('bookmark')} scope="col">Избранное</th>
-                <th /> */}
             </tr>
         </thead>
     );
