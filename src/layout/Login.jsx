882bc15d5dbeb6be2e6871aa5f31b2f182fc0ca0
DmitriyKanischev <116942865+DmitriyKanischev@users.noreply.github.com>
2023-05-23 07:23:53 +0400

    add form and controlled input

--- src/layout/Login.jsx
+++ src/layout/Login.jsx
@@ -1,8 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 const Login = () => {
+    const [email, setEmail] = useState("");
+    const handleChange = (e) => {
+        setEmail(e.target.value)
+        }
     return ( 
         <>
-            <h2>Login</h2>
+            <form>
+                <div>
+                    <label htmlFor='email'>E-mail:</label>
+                    <input type='text' id='email' value={email} onChange={handleChange}/>
+                </div>
+                <div>
+                    <label htmlFor='password'>Password:</label>
+                    <input type='password' id='password'/>
+                </div>
+            </form>
         </>
      );
 }
