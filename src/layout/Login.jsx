edf95f8c132c872dba9a5407de8a745cd9387a6a
DmitriyKanischev <116942865+DmitriyKanischev@users.noreply.github.com>
2023-05-23 07:42:00 +0400

    both inputs controlled, global state

--- src/layout/Login.jsx
+++ src/layout/Login.jsx
@@ -1,19 +1,35 @@
 import React, { useState } from 'react';
 const Login = () => {
-    const [email, setEmail] = useState("");
-    const handleChange = (e) => {
-        setEmail(e.target.value)
+    const [data, setData] = useState({email:"",password:""});
+    const handleChange = ({target}) => {
+        setData((prevState) => ({
+            ...prevState, 
+            [target.name]: target.value
+        }))
+
         }
     return ( 
         <>
             <form>
                 <div>
                     <label htmlFor='email'>E-mail:</label>
-                    <input type='text' id='email' value={email} onChange={handleChange}/>
+                    <input 
+                        type='text' 
+                        id='email' 
+                        name='email' 
+                        value={data.email} 
+                        onChange={handleChange}
+                        />
                 </div>
                 <div>
                     <label htmlFor='password'>Password:</label>
-                    <input type='password' id='password'/>
+                    <input 
+                        type='password' 
+                        id='password' 
+                        name='password'
+                        value={data.password}
+                        onChange={handleChange}
+                        />
                 </div>
             </form>
         </>
