3865be24de11d60b440e051fe129dd51729514d9
DmitriyKanischev <116942865+DmitriyKanischev@users.noreply.github.com>
2023-05-24 22:01:13 +0400

    base validate start

--- src/layout/Login.jsx
+++ src/layout/Login.jsx
@@ -1,16 +1,33 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import TextField from '../components/TextField';
 const Login = () => {
     const [data, setData] = useState({email:"",password:""});
+    const [errors, setErrors] = useState()
     const handleChange = ({target}) => {
         setData((prevState) => ({
             ...prevState, 
             [target.name]: target.value
         }))
         }
+        useEffect(()=>{
+            validate()
+        }, [data])
+
+        const validate = () => {
+            const errors = {}
+            for( const fieldName in data) {
+                if(data[fieldName].trim() === "") {
+                    errors[fieldName] = `${fieldName} обязательно для заполнения`
+                }
+            }
+            setErrors(errors)
+            return Object.keys(errors).length === 0
+        }
 
     const handleSubmit = (e) => {
         e.preventDefault()
+        const isValid = validate()
+        if(!isValid) return;
         console.log(data)
     }
     return ( 
