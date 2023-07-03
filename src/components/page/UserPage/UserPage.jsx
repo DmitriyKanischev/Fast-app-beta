f6c6f577d5d677606aecb1671ca7c7ac76797b2e
DmitriyKanischev <116942865+DmitriyKanischev@users.noreply.github.com>
2023-07-03 22:00:29 +0400

    async for UserPage render

--- src/components/page/UserPage/UserPage.jsx
+++ src/components/page/UserPage/UserPage.jsx
@@ -1,35 +1,48 @@
 import React, {useState, useEffect } from 'react';
 import { Navigate, useNavigate } from 'react-router-dom';
-import PropTypes from "prop-types";
+import PropTypes, { func } from "prop-types";
 import api from "../../../api"
 import QualitiesList from '../../ui/qualities/qualitiesList';
 
 const UserPage = ({userId}) => {
     const navigate = useNavigate()
     const [user, setUser] = useState();
+    const [loading, setLoading] = useState(false)
+
+    async function fetchUsers() {
+        try {
+            setLoading(true)
+            await api.users.getById(userId).then((data) => setUser(data))
+            setLoading(false)
+        } catch (err) {
+            setLoading(false)
+            console.log(err)
+        }
+    }
 
     useEffect(() => {
-        api.users.getById(userId).then((data) => setUser(data))
+        fetchUsers()
     }, []);
 
     const handleBack = () => {
         navigate(-1)
     }
 
-    if(user) {
         return (
             <div>
-                <h1>{user.name}</h1>
-                <h2>Профессия: {user.profession.name}</h2>
-                <QualitiesList qualities={user.qualities} />
-                <h3>Встретился раз: {user.completedMeetings}</h3>
-                <h3>Оценка: {user.rate}</h3>
-                <button onClick={handleBack}>Все пользователи</button>
+                {loading && <h1>loading...</h1>}
+                {user &&
+                <>
+                    <h1>{user.name}</h1>
+                    <h2>Профессия: {user.profession.name}</h2>
+                    <QualitiesList qualities={user.qualities} />
+                    <h3>Встретился раз: {user.completedMeetings}</h3>
+                    <h3>Оценка: {user.rate}</h3>
+                    <button onClick={handleBack}>Все пользователи</button> 
+                </>}
             </div>
         )
-    } else {
-        return <Navigate to='/404'/>            
-    }
+                //need navigate to 404 if !user
 }
 UserPage.propTypes = {
     userId:PropTypes.string.isRequired
